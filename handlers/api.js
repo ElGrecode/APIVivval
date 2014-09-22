835f7d6c5219cf5a6320cf53ffa45e02ad935186
narwhaldo <narwhaldo@gmail.com>
2014-09-22 14:53:38 -0700

    API heatmap permission based on hardcoded API key

--- handlers/api.js
+++ handlers/api.js
@@ -6,32 +6,37 @@ exports.coordinateInformation = function(req, res, next, name){
 };
 
 exports.heatmap = function(req, res){
-  var apiKey = req.params.apiKey;
-  var latlngStrArr = req.params.latlng.split(',');
-  var latlng = [parseFloat(latlngStrArr[0]), parseFloat(latlngStrArr[1])];
-  var zoom = req.params.zoom;
-  // todo: determine zoomRadius based on zoom level
-  var zoomRadius = .000001; // Here we just hardcode it for the time being
-  // todo: incorporate a search term to be searched within query
-  var searchterm = req.params.searchterm;
+  if (req.params.apiKey == "jdfs8A3298Clkp62kH0uf29h29"){ 
+    console.log('permission granted');
+    var latlngStrArr = req.params.latlng.split(',');
+    var latlng = [parseFloat(latlngStrArr[0]), parseFloat(latlngStrArr[1])];
+    var zoom = req.params.zoom;
+    // todo: determine zoomRadius based on zoom level
+    var zoomRadius = .000001; // Here we just hardcode it for the time being
+    // todo: incorporate a search term to be searched within query
+    var searchterm = req.params.searchterm;
 
-  // The radius of our query is determined by the latlng and zoom of the input
-  // Let's start by querying +/-.000001 of our latlng
+    // The radius of our query is determined by the latlng and zoom of the input
+    // Let's start by querying +/-.000001 of our latlng
 
 
-  var query = Blazon.find()
-  .where('geolocation.lat').gt(latlng[0] - zoomRadius).lt(latlng[0] + zoomRadius)
-  .where('geolocation.lng').gt(latlng[1] - zoomRadius).lt(latlng[1] + zoomRadius)
-  .limit(400)
-  .exec(function(err, blazons){
-    // create the json data for our API endpoint
-    res.json({
-      latlng: latlng,
-      searchTerm: searchterm,  
-      zoom: zoom,
-      blazons: blazons,
+    var query = Blazon.find()
+    .where('geolocation.lat').gt(latlng[0] - zoomRadius).lt(latlng[0] + zoomRadius)
+    .where('geolocation.lng').gt(latlng[1] - zoomRadius).lt(latlng[1] + zoomRadius)
+    .limit(400)
+    .exec(function(err, blazons){
+      // create the json data for our API endpoint
+      res.json({
+        latlng: latlng,
+        searchTerm: searchterm,  
+        zoom: zoom,
+        blazons: blazons,
+      });
     });
-  });
+  } else {
+    console.log("You don't have permission to access the API");
+    res.redirect('/blaze');
+  }
   
 
 	
