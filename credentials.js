2efac3f19265da3ae9dd7316e4c9521a48f03276
narwhaldo <narwhaldo@gmail.com>
2014-09-22 21:42:14 -0700

    exposes credentials because of new privacy

---
+++ credentials.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mongo: {
+    development: {
+      connectionString: 'mongodb://striatum:3coe4ln@ds039020.mongolab.com:39020/vivval'
+    }, production: {
+      connectionString: 'mongodb://striatum:3coe4ln@ds039020.mongolab.com:39020/vivval'
+    }
+  }
+}
